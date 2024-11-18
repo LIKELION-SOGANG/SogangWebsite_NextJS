d693ab974f13dfc3d0d11eba9d692eaba0bb88c2
inyoung <rmdnps10@gmail.com>
2024-11-18 21:34:55 +0900

    Feat: add Clarity and GA components for metrics tracking #109

---
+++ src/app/metrices/Clarity.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Clarity() {
+  return <div>Clarity</div>
+}
---
+++ src/app/metrices/index.js
@@ -0,0 +1,11 @@
+import Clarity from './Clarity'
+import GA from './GA'
+
+const Metrics = () => (
+  <>
+    <Clarity />
+    <GA />
+  </>
+)
+
+export default Metrics
